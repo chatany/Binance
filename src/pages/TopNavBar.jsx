c4483906c4c17ce9053b0d2c7fda2cf56b67acea
chatany <chatany2003@gmail.com>
2025-08-02 14:28:04 +0530

    workij

--- src/pages/TopNavBar.jsx
+++ src/pages/TopNavBar.jsx
@@ -68,7 +68,7 @@ export const TopNav = ({
         dark ? "bg-[#181A20] text-[#EAECEF] " : "bg-white text-black "
       }`}
     >
-      <div className="flex  w-full max-w-[70%] items-center text-lg gap-2 font-semibold leading-6 lg:justify-evenly">
+      <div className="flex xl:w-[60%] items-center text-lg gap-2 font-semibold leading-6 lg:justify-evenly">
         <div className="text-amber-400 font-semibold text-xl">
           {dark ? (
             <img src="/bitzup_light_logo.png" className="h-10 w-30 sm:w-36" />
