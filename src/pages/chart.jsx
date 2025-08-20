2a12127f7ee65362a798caa5464c97c0ce90c68c
chatany <chatany2003@gmail.com>
2025-08-20 10:01:29 +0530

    ego is better then you

--- src/pages/chart.jsx
+++ src/pages/chart.jsx
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
+
 export const ChartEmbed = ({ searchQuery }) => {
   const query = searchQuery || "BTCUSDT";
+  const apiId  = useSelector((state) => state.counter.apiId);
+  const url=`https://chart.bitzup.com/${apiId==="bitget"?"v2":"v1"}/?market=${query}`
+  
   return (
     <div className="max-w-full h-full rounded-lg">
       <iframe
-        src={`https://chart.bitzup.com/v1/?market=${query}`}
+        src={url}
         title="BTCUSDT Chart"
         width={"100%"}
         height={"100%"}
