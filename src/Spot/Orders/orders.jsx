8dfd9f854a7d7503c136cece6725ae245b462fde
chatany <chatany2003@gmail.com>
2025-10-03 14:48:25 +0530

    aaa

--- src/Spot/Orders/orders.jsx
+++ src/Spot/Orders/orders.jsx
@@ -7,9 +7,9 @@ import { setIsSuccess, setShowPopup } from "../../store/webSocket";
 import { ScaleLoader } from "react-spinners";
 import { ModifyPopup } from "./Modify/popup";
 import { FaRegEdit } from "react-icons/fa";
-import { ConfirmationBox } from "/src/common/DeletePopup.jsx";
 import { useDeviceInfo } from "../../hooks/useDeviceInfo";
 import { Loder } from "../../common/Loder";
+import { ConfirmationBox } from "../../common/deletePopup";
 
 export const OpenOrders = () => {
   const openOrder = useSelector((state) => state.counter.openOrder);
